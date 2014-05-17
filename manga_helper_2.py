51077cd2732fec51c12606a03e0c40fc92922d6e
pythonerjing <pythoner.jing@gmail.com>
2014-05-17 14:24:18 +0800

    5/17 14:24

--- manga_helper_2.py
+++ manga_helper_2.py
@@ -6,10 +6,10 @@ import urllib2, urllib
 import re
 import threading, Queue, time, random
 from sgmllib import SGMLParser
+import socket
 
 
 root_url_img = "http://imgfast.dmzj.com/"
-url_test = "http://manhua.dmzj.com/dqjsdsd/13762.shtml" 
 url_root = "http://manhua.dmzj.com"
 
 #图片名称规则
@@ -22,6 +22,8 @@ reg_img_type = re.compile(r"(\w+)")
 path_cur = os.path.abspath(".")
 
 js_cxt = PyV8.JSContext()
+		
+#socket.setdefaulttimeout(10)#设置超时10s
 
 #解析卷
 class ParserChapter(SGMLParser):
@@ -120,47 +122,58 @@ class Block:
 		self.img_url = img_url
 		self.location = location
 
+	def count(self):
+		self.task.count()
+
 #下载任务
 class Task:
-	def __init__(self, manga, selection, ui):
+	def __init__(self, manga, selection, queue, queue2):
 		#任务对应的漫画对象
 		self.manga = manga
 		#任务选择
 		self.selection = selection
 		#任务对应的ui组件
-		self.ui = ui
+		self.ui = None 
 		#任务块
 		self.blocks = [] 
 		#已下载的页数
 		self.downloaded = 0
 		#总页数
-		self.num = 0
+		self.page_num = 0
+		#线程容器
+		self.queue = queue
+		#低优先级线程容器
+		self.queue2 = queue2
 
 	#下载计数及ui刷新
 	def count(self):
-		self.downloaded += 1
+		self.ui.count()
+
+	def run_task(self):
+		map(lambda x : self.queue.put(x), self.blocks)
 
 	#启动下载任务
-	def run_task(self, selection):
-		self.selection = selection	
+	def parse(self):
 		path_manga = os.path.join(path_cur, self.manga.name).decode("utf-8")
 		if not os.path.exists(path_manga):
 			os.mkdir(path_manga)
-		for chapter, url in selection:
-			path_chapter = os.path.join(path_manga, chapter).decode("utf-8")
+		else:
+			return False
+		for chapter, url in self.selection:
+			path_chapter = os.path.join(path_manga.encode("utf-8"), chapter).decode("utf-8")
 			if not os.path.exists(path_chapter):
 				os.mkdir(path_chapter)
 			socket = urllib2.urlopen(url)
 			content = socket.read()
 			socket.close()
 			img_urls = Fetch.fetch_img(content)
-			self.num += len(img_urls)
+			self.page_num += len(img_urls)
 			for i, img_url in enumerate(img_urls):
-				pic_name = ".".join(str(i + 1), Fetch.fetch_type(img_url)) 
+				pic_name = ".".join([str(i + 1), Fetch.fetch_type(img_url)]) 
 				location = os.path.join(path_chapter, pic_name)
 				self.blocks.append(Block(self, img_url, location))
 
-		map(lambda x : queue.put(x), self.blocks)
+		return True
 
 #漫画类
 class Manga:
@@ -184,20 +197,26 @@ class Manga:
 		self.chapter_url = chapter_parser.get_chapter_url()
 
 class Thread(threading.Thread):
-	def __init__(self, queue, handler):
+	def __init__(self, queue, queue2, handler):
 		threading.Thread.__init__(self)
 		self.queue = queue
+		self.queue2 = queue2
 		self.handler = handler
 
 	def run(self):
 		time.sleep(1)
-		while self.queue.qsize() > 0:
-			self.handler(self.queue.get())
+		while True:
+			if (self.queue.qsize() > 0 or self.queue2.qsize() == 0):
+				self.handler(self.queue.get())
+			else:
+				print "get from queue2"
+				self.handler(self.queue2.get())
 
 #下载处理器
 def handler_download(block):
 	try:
 		urllib.urlretrieve(block.img_url, block.location)
+		print "queue.qsize():", block.task.queue.qsize(), "queue2.qsize():", block.task.queue2.qsize()
 		block.count()
 	except Exception, e:
-		print e
+		block.task.queue2.put(block)
--- ui.py
+++ ui.py
@@ -6,8 +6,9 @@ import  wx.lib.scrolledpanel as scrolled
 
 #队列
 queue = None
+queue2 = None
 #线程池大小
-NUM_THREAD = 50
+NUM_THREAD = 10
 #线程池
 thread_pool = []
 
@@ -26,17 +27,22 @@ class ScrollChapter(scrolled.ScrolledPanel):
 	def __init__(self, parent, id = -1, pos = wx.DefaultPosition, size = wx.DefaultSize):
 		scrolled.ScrolledPanel.__init__(self, parent, id, pos, size, wx.BORDER)
 
-		self.sizer = wx.FlexGridSizer(rows = 500, cols = 1, hgap = 0, vgap = 0)
+		self.sizer = wx.FlexGridSizer(rows = 1000, cols = 1, hgap = 0, vgap = 0)
 		self.SetSizer(self.sizer)
 		self.SetBackgroundColour("white")
+		self.checkboxs = []
+		self.chapter_urls = None
 
-	def add_chapter(self, chapters):
+	def set_chapter(self, chapter_urls):
+		self.chapter_urls = chapter_urls
 		self.sizer.Clear(True)	
+		self.checkboxs = []
 		width = 0
 		height = 0
-		for c in chapters:
+		for c, u in chapter_urls:
 			try:
 				checkbox = wx.CheckBox(self, -1, c.decode("utf-8"))
+				self.checkboxs.append(checkbox)
 				if width == 0 or height == 0:
 					width, height = checkbox.GetSize()
 				self.sizer.Add(checkbox, 0, wx.LEFT | wx.TOP, 5)
@@ -44,12 +50,56 @@ class ScrollChapter(scrolled.ScrolledPanel):
 			except Exception, e:
 				pass
 
-		self.SetScrollbars(1, 1, width, height * len(chapters) + 3 * (len(chapters) - 1))
+		self.SetScrollbars(1, 1, width, height * len(chapter_urls) + 5 * (len(chapter_urls) - 1))
 
-class PanelLeft(wx.Panel):
+	def select_all(self, value):
+		map(lambda x : x.SetValue(value), self.checkboxs)
+
+	def select_reverse(self):
+		map(lambda x : x.SetValue(not x.GetValue()), self.checkboxs)
+
+	def get_selected(self):
+		rs = []
+		for i, c in enumerate(self.checkboxs):
+			if c.GetValue(): 
+				rs.append(self.chapter_urls[i])
+		return rs
+
+class ScrollTask(scrolled.ScrolledPanel):
 	def __init__(self, parent, id = -1, pos = wx.DefaultPosition, size = wx.DefaultSize):
+		scrolled.ScrolledPanel.__init__(self, parent, id, pos, size, style = wx.BORDER)
+		self.SetBackgroundColour("white")
+		self.sizer = wx.FlexGridSizer(rows = 1000, cols = 1, hgap = 0, vgap = 5)
+		self.SetSizer(self.sizer)
+
+	def add(self, panel_task):
+		self.sizer.Add(panel_task, 1, wx.LEFT | wx.TOP, 5)
+		self.sizer.Layout()
+		length = self.sizer.GetItemCount() + 1
+		width, height = panel_task.GetSize()
+		self.SetScrollbars(1, 1, width + 2 * 5, height * length + 5 * (length - 1))
+
+class PanelTask(wx.Panel):
+	def __init__(self, parent, task, id = -1, pos = wx.DefaultPosition, size = wx.DefaultSize):
 		wx.Panel.__init__(self, parent, id, pos, size)
-		self.scroll_1 = ScrollChapter(self, -1, (0, 0), (150, 200))
+		self.text_1 = wx.StaticText(self, -1, task.manga.name.decode("utf-8"), pos = (5, 5))
+		self.text_2 = wx.StaticText(self, -1, u"", pos = (5, 25))
+		self.gauge = wx.Gauge(self, -1, task.page_num, pos = (0, 45), size = (207, -1))
+		self.gauge.SetValue(0)
+		self.SetBackgroundColour("#66ccff")
+		self.value = 0
+		task.ui = self
+		self.page_num = task.page_num
+		rate = "%.1f" % (float(self.value) / self.page_num * 100)
+		info = "%d / %d (%s%%)" % (self.value, self.page_num, rate)  
+		self.text_2.SetLabel(info)
+
+	def count(self):
+		self.value += 1
+		rate = "%.1f" % (float(self.value) / self.page_num * 100)
+		info = "%d / %d (%s%%)" % (self.value, self.page_num, rate)  
+		self.text_2.SetLabel(info)
+		self.gauge.SetValue(self.value)
 
 class PanelMain(wx.Panel):
 	def __init__(self, parent, id = -1, pos = wx.DefaultPosition, size = wx.DefaultSize):
@@ -57,18 +107,43 @@ class PanelMain(wx.Panel):
 
 		self.ctrl = wx.TextCtrl(self, -1, "", (10, 10), (280, -1))
 		self.btn_1 = wx.Button(self, -1, u"链接", (300, 7), (-1, -1))
-		self.panel_left = ScrollChapter(self, -1, (10, 50), (150, 230))
-		self.checkbox_1 = wx.CheckBox(self, -1, u"全选", (10, 290))
-		self.checkbox_2 = wx.CheckBox(self, -1, u"反选", (10, 310))
-		self.btn_2 = wx.Button(self, -1, u"下载", (60, 290), (100, 37))
+		self.chapter_scroll = ScrollChapter(self, -1, (237, 45), (150, 235))
+		self.task_scroll = ScrollTask(self, -1, (10, 45), (220, 280))
+		self.checkbox_1 = wx.CheckBox(self, -1, u"全选", (237, 290))
+		self.checkbox_2 = wx.CheckBox(self, -1, u"反选", (237, 310))
+		self.btn_2 = wx.Button(self, -1, u"下载", (287, 290), (100, 37))
 
 		self.btn_1.Bind(wx.EVT_BUTTON, self.OnLink)
+		self.checkbox_1.Bind(wx.EVT_CHECKBOX, self.OnSelectAll)
+		self.checkbox_2.Bind(wx.EVT_CHECKBOX, self.OnSelectReverse)
+		self.btn_2.Bind(wx.EVT_BUTTON, self.OnDownload)
+
+		self.manga = None
+
+	def OnSelectAll(self, evt):
+		self.chapter_scroll.select_all(self.checkbox_1.GetValue())	
+
+	def OnSelectReverse(self, evt):
+		self.chapter_scroll.select_reverse()
 
 	def OnLink(self, evt):
-		manga = MH.Manga(self.ctrl.GetValue())
-		manga.parse()
-		chapter = [chapter for chapter, url in manga.chapter_url]
-		self.panel_left.add_chapter(chapter)
+		self.manga = MH.Manga(self.ctrl.GetValue())
+		self.manga.parse()
+		self.chapter_scroll.set_chapter(self.manga.chapter_url)
+		self.checkbox_1.SetValue(False)
+		self.checkbox_2.SetValue(False)
+
+	def OnDownload(self, evt):
+		selection = self.chapter_scroll.get_selected()
+		task = MH.Task(self.manga, selection, queue, queue2)
+		if not task.parse():
+			info = "《%s》下载过了，请转移同名文件夹以开始新的下载" % (self.manga.name)
+			dialog = wx.MessageDialog(self, info.decode("utf-8"), u"提示", wx.OK | wx.ICON_INFORMATION)
+			dialog.ShowModal()
+			return
+		panel_task = PanelTask(self.task_scroll, task)
+		self.task_scroll.add(panel_task)
+		task.run_task()
 
 class FrameMain(wx.Frame):
 	def __init__(self, parent, id, title):
@@ -78,13 +153,14 @@ class FrameMain(wx.Frame):
 
 		self.Show()
 		self.Center()
-		self.SetClientSize((400, 335))
+		self.SetClientSize((395, 335))
 		self.Fit()
 
 def init_thread_pool():
-	global queue
+	global queue, queue2
 	queue = Queue.Queue(0)
-	map(lambda x : thread_pool.append(MH.Thread(queue, MH.handler_download)), xrange(NUM_THREAD))
+	queue2 = Queue.Queue(0)
+	map(lambda x : thread_pool.append(MH.Thread(queue, queue2, MH.handler_download)), xrange(NUM_THREAD))
 	map(lambda x : x.setDaemon(True), thread_pool)
 	map(lambda x : x.start(), thread_pool)
 
